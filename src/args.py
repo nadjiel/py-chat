a02ae0c5552b22aa1521228f637014a1ff07dc2e
Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
2024-08-01 14:49:41 -0300

    :sparkles: feat: adicionando interpretação de comandos

--- src/args.py
+++ src/args.py
@@ -1,5 +1,28 @@
 from argparse import ArgumentParser
 
+def help(data: dict) -> dict:
+    return data
+
+def nick(data: dict) -> dict:
+    return data
+
+def users(data: dict) -> dict:
+    return data
+
+def sendmsg(data: dict) -> dict:
+    return data
+
+def msg(data: dict) -> dict:
+    return data
+
+commands = {
+    "!help": help,
+    "!nick": nick,
+    "!users": users,
+    "!sendmsg": sendmsg,
+    "!msg": msg,
+}
+
 def get_args():
     parser = ArgumentParser()
 
@@ -16,3 +39,28 @@ def get_args():
     )
 
     return parser.parse_args()
+
+def is_command(msg: str) -> bool:
+    return msg.startswith('!')
+
+def extract_prefix(command: str) -> str:
+    return command.split(None, 1)[0]
+
+def is_valid_command(command: str) -> bool:
+    prefix = extract_prefix(command)
+
+    if prefix in commands.keys(): return True
+
+    return False
+
+def handle_command(data: dict, command: str):
+    if not is_valid_command(command):
+        print(command + " não é um comando válido, use o comando !help para ajuda.")
+        return data
+
+    prefix = extract_prefix(command)
+
+    return commands[prefix](data)
+
+def get_nick():
+    input()
