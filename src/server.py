2ea844d3ca39b3348a42f75ebbe9a3f5a2a54401
Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
2024-08-01 16:44:11 -0300

    :sparkles: feat: organizando a função de comunicação

--- src/server.py
+++ src/server.py
@@ -2,36 +2,56 @@ import socket
 from args import get_args
 from threading import Thread
 
-connections = []
+from args import handle_command
+
+connections = {}
 threads = []
 
+def create_connection(thread: Thread) -> dict:
+    return {
+        "thread": thread,
+        "data": {},
+    }
+
 def communicate(client: socket, address):
-    print("New connection from: " + str(address))
-    print("socket:")
-    print(client)
+    print("Nova conexão de: " + str(address))
+
+    connection = connections[str(address)]
+
+    print(connection)
 
     while True:
         data = client.recv(1024).decode()
 
         if not data: break
+
+        client_input = str(data)
+
+        connection["data"] = handle_command(client, connection["data"], client_input)
+
+        print(connection)
         
-        print("from connected user: " + str(data))
-        data = input(' -> ')
-        client.send(data.encode())  # send data to the client
+        # print("from connected user: " + str(data))
+        # data = input(' -> ')
+        # client.send(data.encode())  # send data to the client
 
     client.close()
 
 def start(host: str, port: int):
-    server_socket = socket.socket()
+    server = socket.socket()
 
-    server_socket.bind((host, port))
-    server_socket.listen()
+    server.bind((host, port))
+    server.listen()
 
     while True:
-        client_socket, address = server_socket.accept()
+        client_socket, address = server.accept()
 
         thread = Thread(target=communicate, args=(client_socket, address))
         thread.start()
+
+        connections[str(address)] = create_connection(thread)
+        # connections.append(create_connection(thread))
+
         threads.append(thread)
 
         # thread.join()
