99528624779099ec0c78fd4961c72f4d042dd60b
Daniel de Oliveira <oliveira.daaaaniel@gmail.com>
2024-08-09 11:03:59 -0300

    :sparkles: feat: separando a thread de espera de conexões

--- src/server.py
+++ src/server.py
@@ -4,6 +4,7 @@ from threading import Thread
 
 from args import handle_command
 
+command = ""
 connections = {}
 
 def close_connections() -> None:
@@ -51,49 +52,44 @@ def communicate(client: socket, address):
     del connections[address]
     client.close()
 
+def accept_connections(server: socket) -> None:
+    while command != "!exit":
+        client_socket = None
+        address = None
+
+        try:
+            client_socket, address = server.accept()
+        except:
+            return
+
+        thread = Thread(target=communicate, args=(client_socket, address))
+        
+        connections[address] = create_connection(thread, client_socket)
+
+        thread.start()
+
 def start(host: str, port: int):
     server = socket.socket()
 
     server.bind((host, port))
     server.listen()
 
+    thread = Thread(target=accept_connections, args=(server,))
+    thread.start()
+
+    print("Esperando por conexões...")
+    print("Use !exit para encerrar o servidor.")
+
     while True:
-        client_socket, address = server.accept()
+        command = input().strip().lower()
 
-        # print("address: ")
-        # print(address)
-        # print("sockname: ")
-        # print(client_socket.getsockname())
-        # print("peername: ")
-        # print(client_socket.getpeername())
-
-        thread = Thread(target=communicate, args=(client_socket, address))
-        thread.start()
-
-        connections[address] = create_connection(thread, client_socket)
-        #print(connections)
-        # connections.append(create_connection(thread))
-
-        # thread.join()
+        if command != "!exit":
+            print("Comando inválido!")
+        else:
+            break
     
-    # print(client_socket)
-    # print(address)
-
-    # print("New connection from: " + str(address))
-
-    # while True:
-    #     # receive data stream. it won't accept data packet greater than 1024 bytes
-    #     data = client_socket.recv(1024).decode()
-
-    #     if not data:
-    #         # if data is not received break
-    #         break
-        
-    #     print("from connected user: " + str(data))
-    #     data = input(' -> ')
-    #     client_socket.send(data.encode())  # send data to the client
-
-    # client_socket.close()  # close the connection
+    close_connections()
+    server.close()
 
 
 if __name__ == '__main__':
